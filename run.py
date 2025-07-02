12e771d4407b7987c7de77a4551a4cad90feb794
Júlio C Oliveira <julio3.oliveir@gmail.com>
2025-07-02 09:34:48 -0300

    feat(run): Adiciona teste completo e funcional.

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ MUTATION_RATE:float = 0.03
 
 NUMBER_OF_GENERATIONS: int = 1000
 MIN_VALUE: int = 0
-RANDOM_STATE: int = 42
+RANDOM_STATE: int = 41
 # 41 termina de primeira
 
 generate_population = PopulationGenerator.generate_eight_queen_vector
@@ -15,7 +15,8 @@ stopping_criterion = StoppingCriteria.stop_eight_queen_vector_min
 parent_selection_strategy = ParentSelector.select_parent_roulette_eight_queen_vector
 crossover_strategy = CrossoverMethods.cut_point_eight_eight_queen_vector
 mutation_strategy = Modifier.apply_bit_flip_eight_queen_vector
-survivor_selection_strategy = ...
+survivor_selection_strategy = SuvivorCriteria.random_switch_all_population_eight_queen_vector
+
 endRound = 0
 population = generate_population(POPULATION_SIZE, RANDOM_STATE)
 evaluates = evaluate_population(population)
@@ -27,12 +28,14 @@ for round in range(NUMBER_OF_GENERATIONS):
         break
 
     parents = parent_selection_strategy(population, evaluates, round, RANDOM_STATE)
-    sons = crossover_strategy(parents, CROSSOVER_RATE, round)
+    sons = crossover_strategy(parents, CROSSOVER_RATE, round, RANDOM_STATE)
     mutateSons = mutation_strategy(sons, MUTATION_RATE, round, RANDOM_STATE)
     sonsEvaluates = evaluate_population(mutateSons)
 
-    newGeneration =  ...
+    population, evaluates =  survivor_selection_strategy(population, mutateSons, evaluates, sonsEvaluates, POPULATION_SIZE, round, RANDOM_STATE)
 
-print(f"\nPopulação Final alcançada no round {endRound}:")
+print(f"\nPopulação Final alcançada no round {endRound}, número de indivíduos: {len(population)}:")
 for i in range(len(population)):
     print(f"Indivíduo: {population[i]} | Pontuação: {evaluates[i]}")
+
+print()
